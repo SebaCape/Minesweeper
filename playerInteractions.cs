527b285b71b11b0ea96ad34d3a9cf19da5ad9822
SebaCape <scapellan55@gmail.com>
2023-06-26 09:40:37 -0400

    10th commit, barebones functionality for player interactions method implemented, polishing and game loop coming after

--- playerInteractions.cs
+++ playerInteractions.cs
@@ -16,16 +16,46 @@ namespace Minesweeper
        Console.WriteLine("cell already active");
       }
 
-      //determines which cell of the board will be flagged
-      public void cellFlag()
-      {
+     //checks whether specific cell is a mine or is flagged, if not, it will display the cells adjacent mines variable
+     public void cellCheck(int x, int y, gameBoard g)
+     {
+      if(g.board[x,y].isMine == true && g.board[x,y].isActive == true)
+      gameOver = 1;
+     }
 
+      //determines which cell of the board will be flagged
+      public void cellFlag(int x, int y, gameBoard g)
+      {
+       if(g.board[x,y].isFlagged != true)
+       g.board[x,y].isFlagged = true;
+       else
+       g.board[x,y].isFlagged = false;
       }
 
       //determines what will be done with that board cell
-      public void actionSelect()
+      public void actionSelect(string input, int x, int y, gameBoard g)
       {
-       
+       Console.WriteLine("Choose the x coordinate");
+       x = Convert.ToInt32(Console.ReadLine());
+       Console.WriteLine("Choose the y coordinate");
+       y = Convert.ToInt32(Console.ReadLine());
+
+       Console.WriteLine("Choose which action you would like to perform. Flag: F, Activate: A");
+       input = Console.ReadLine();
+       switch (input)
+       {
+       case "F":
+       cellFlag(x,y,g);
+       break;
+
+       case "A":
+       cellActivate(x,y,g);
+       break;
+
+       default:
+       actionSelect("",0,0,g);
+       break;
+       }
       }
 
       //determines whether the game is over or not, additionally determines if player has won or lost 
